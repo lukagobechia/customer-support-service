f775f87339e1960425d7fdf4ac8643d1dcfb11fa
Luka Gobechia <114133823+lukagobechia@users.noreply.github.com>
2025-04-27 22:39:10 +0400

    Update api-service.js base url

--- chat-client/src/lib/api-service.js
+++ chat-client/src/lib/api-service.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://customer-support-service-5lst.onrender.com";
 
 const ApiService = {
   login: async (email, password) => {
